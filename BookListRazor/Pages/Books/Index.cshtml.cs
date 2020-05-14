31a3dec18581c19581a89618a04b1cb4b4c94dfa
NehemiahLimo <nehemiahlimo02@gmail.com>
2020-05-14 09:42:42 +0300

    Add project files.

---
+++ BookListRazor/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookListRazor.Model;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet() => Books = await _db.Book.ToListAsync();
+    }
+}
